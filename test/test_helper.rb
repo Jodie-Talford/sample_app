659bcb96331c18a347e955d4ccabb3cfce36ba1a
jodie <jodie@jaguardesignstudio.com>
2022-08-01 08:42:22 -0700

    add a full_title helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # require 'rails/test_help'
 require 'test/unit/rails/test_help'
+# Although many systems will show the appropriate colors for red and green
+# test suites, adding minitest reporters lends a pleasant polish to test outputs
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
