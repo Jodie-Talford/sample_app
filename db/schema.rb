8ab0ea029fa59f3d814ab51596af5d05690c74aa
jodie <jodie@jaguardesignstudio.com>
2022-08-03 15:13:36 -0700

    USE SSL and the Puma webserver in production

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_08_02_195053) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
