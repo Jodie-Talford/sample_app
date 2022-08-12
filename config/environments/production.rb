c3720fc0c387b022ad273aab7adb5de4985950f7
jodie <jodie@jaguardesignstudio.com>
2022-08-12 10:05:24 -0700

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,8 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-
+  # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
